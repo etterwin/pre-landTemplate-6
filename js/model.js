890758128dff0d3dcbe51e25b67534884f572d40
Daria Plotnikova <etterwin.yll@gmail.com>
2017-09-27 11:37:56 +0300

    add js

---
+++ js/model.js
@@ -0,0 +1,7 @@
+    /*'Mozilla/5.0 (Linux; U; Android 4.0.3; en-in; SonyEricssonMT11i' +
+    ' Build/4.1.A.0.562) AppleWebKit/534.30 (KHTML, like Gecko)' +
+    ' Version/4.0 Mobile Safari/534.30';*/
+
+var md = new MobileDetect(window.navigator.userAgent);
+
+document.getElementById('model').innerHTML = md.mobile();
